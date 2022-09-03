1ffcb1ad660d624864339c299d2818164ead5222
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2022-09-03 12:13:50 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (51.28%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.cpp
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -5,17 +5,17 @@ class Solution
         {
             int count = 0;
             int end = s.length() - 1;
-            while (s[end] == ' ' && end>=0)
+            while (s[end] == ' ' && end >= 0)
             {
                 end--;
-                if(end<0) return count;
+                if (end < 0) return count;
             }
 
-            while (s[end] != ' ' && end>=0)
+            while (s[end] != ' ' && end >= 0)
             {
                 count++;
                 end--;
-                if(end < 0) return count;
+                if (end < 0) return count;
             }
             return count;
         }
