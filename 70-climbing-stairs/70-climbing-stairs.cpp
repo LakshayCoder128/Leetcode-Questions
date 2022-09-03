d2c649a3821a5c13f61bbafeba0db90b8ebcb7b1
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2022-09-03 16:34:12 +0530

    Time: 2 ms (42.67%), Space: 6 MB (57.17%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,11 @@
+class Solution
+{
+    public:
+        int climbStairs(int n)
+        {
+            long long a = 1, b = 1;
+            while (n--)
+                a = (b += a) - a;
+            return (int)a;
+        }
+};
