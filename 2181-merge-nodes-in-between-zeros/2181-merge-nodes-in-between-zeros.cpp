5d908bb686be04d3984ab93a3866942c863948aa
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2022-09-05 00:10:34 +0530

    Time: 827 ms (86.46%), Space: 274.7 MB (12.42%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
@@ -1,40 +1,45 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *    int val;
+ *    ListNode * next;
+ *    ListNode() : val(0), next(nullptr) {}
+ *    ListNode(int x) : val(x), next(nullptr) {}
+ *    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *};
  */
-class Solution {
-public:
-    ListNode* mergeNodes(ListNode* head) {
-        ListNode* ans = new ListNode();
-        ListNode* t = head;
-        ListNode* p = ans;
-        int count =0;
-         int sum ;
-        while(t!=NULL){
-            // cout<<t->val<<endl;
-            if(t->val == 0) {
-                // cout<<"Sum: "<<sum<<endl;
-                if(count!=0){
-                    p->next = new ListNode(sum);
-                    p=p->next;
+class Solution
+{
+    public:
+        ListNode* mergeNodes(ListNode *head)
+        {
+            ListNode *ans = new ListNode();
+            ListNode *t = head;
+            ListNode *p = ans;
+            int count = 0;
+            int sum;
+            while (t != NULL)
+            {
+               	// cout<<t->val<<endl;
+                if (t->val == 0)
+                {
+                   	// cout<<"Sum: "<<sum<<endl;
+                    if (count != 0)
+                    {
+                        p->next = new ListNode(sum);
+                        p = p->next;
+                    }
+                    sum = 0;
+                    count++;
                 }
-                sum =0;
-                count++;
+                else
+                {
+                   	// cout<<t->val<<endl;
+                    sum += t->val;
+                }
+
+                t = t->next;
             }
-            else{
-                // cout<<t->val<<endl;
-                sum+= t->val;
-            }
-            
-            t=t->next;
-            
+            return ans->next;
         }
-        return ans->next;
-    }
 };
