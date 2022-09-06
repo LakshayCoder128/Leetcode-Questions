623931927bf30edea64344f9090b8a0f6ddcf420
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2022-09-07 00:17:39 +0530

    Time: 15 ms (33.68%), Space: 13.7 MB (26.41%) - LeetHub

---
+++ 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.cpp
@@ -0,0 +1,39 @@
+class Solution
+{
+    public:
+        bool checkDistances(string s, vector<int> &distance)
+        {
+            map<char, int> m;
+            for (int i = 0; i < s.length(); i++)
+            {
+                m[s[i]]++;
+            }
+
+            for (int i = 0; i < s.length(); i++)
+            {
+                int index = s[i] - 'a';
+                if (m[s[i]] > 1)
+                {
+                    for (int j = i + 1; j < s.length(); j++)
+                    {
+                        if (s[i] == s[j])
+                        {
+                            if (distance[index] != j - i - 1)
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+
+                    if (distance[index] != 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+};
