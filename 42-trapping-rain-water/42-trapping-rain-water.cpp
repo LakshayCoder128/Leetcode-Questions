5f8f38957e62eb431f790007c6af95e3c3226f32
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2022-09-18 23:53:20 +0530

    Time: 125 ms (5.15%), Space: 20.4 MB (40.49%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -3,17 +3,22 @@ class Solution
     public:
         int trap(vector<int> &height)
         {
+//             Stack creation
             stack<int> s;
             int total = 0;
 
             for (int i = 0; i < height.size(); i++)
             {
+                
                 while (!s.empty() && height[s.top()] < height[i])
                 {
+                cout<<"Top : "<< s.top()<<endl;
+                cout<<" height [i] : "<<height[i]<<endl;
+                    
                     int removedInd = s.top();
                     s.pop();
                     if (s.empty()) break;
-
+                    
                     int heightVal = min(height[i], height[s.top()]) - height[removedInd];
                     int length = i - s.top() - 1;
                     total += heightVal * length;
