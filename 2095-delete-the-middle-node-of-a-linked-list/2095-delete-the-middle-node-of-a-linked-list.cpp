bb59bdd3a9a8fde982a0265529f2c40b6228f002
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2022-10-14 12:39:53 +0530

    Time: 2083 ms (43.38%), Space: 294.8 MB (64.88%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -1,11 +1,11 @@
 /**
  *Definition for singly-linked list.
  *struct ListNode {
- *    int val;
- *    ListNode * next;
- *    ListNode() : val(0), next(nullptr) {}
- *    ListNode(int x) : val(x), next(nullptr) {}
- *    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *  int val;
+ *  ListNode * next;
+ *  ListNode() : val(0), next(nullptr) {}
+ *  ListNode(int x) : val(x), next(nullptr) {}
+ *  ListNode(int x, ListNode *next) : val(x), next(next) {}
  *};
  */
 class Solution
@@ -13,15 +13,37 @@ class Solution
     public:
         ListNode* deleteMiddle(ListNode *head)
         {
-            if(head->next == NULL){
+           	//             if (head->next == NULL)
+           	//             {
+           	//                 return NULL;
+           	//             }
+           	//             ListNode *pre = head;
+           	//             ListNode *slow = head;
+           	//             ListNode *fast = head;
+           	//             while (fast != NULL && fast->next != NULL)
+           	//             {
+           	//                 pre = slow;
+           	//                 slow = slow->next;
+           	//                 fast = fast->next;
+           	//                 if (fast->next)
+           	//                 {
+           	//                     fast = fast->next;
+           	//                 }
+           	//             }
+
+           	//             pre->next = slow->next;
+           	//             delete(slow);
+           	//             return head;
+
+            if (head->next == NULL)
+            {
                 return NULL;
             }
-            ListNode *pre = head;
+
             ListNode *slow = head;
-            ListNode *fast = head;
+            ListNode *fast = head->next->next;
             while (fast != NULL && fast->next != NULL)
             {
-                pre = slow;
                 slow = slow->next;
                 fast = fast->next;
                 if (fast->next)
@@ -30,8 +52,8 @@ class Solution
                 }
             }
 
-            pre->next = slow->next;
-            delete(slow);
+            slow->next = slow->next->next;
+
             return head;
         }
 };
