5b2811031acaa7080f47cf69e6d485c2ae56e655
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2022-10-17 12:42:50 +0530

    Time: 6 ms (47.48%), Space: 6.2 MB (98.63%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -3,23 +3,36 @@ class Solution
     public:
         bool checkIfPangram(string sentence)
         {
-            if (sentence.size() < 26)
+           	// if (sentence.size() < 26)
+           	// {
+           	//     return false;
+           	// }
+           	// int arr[26] = { 0 };
+           	// for (int i = 0; i < sentence.length(); i++)
+           	// {
+           	//     int word = sentence[i] - 'a';
+           	//     arr[word]++;
+           	// }
+           	// for (int i = 0; i < 26; i++)
+           	// {
+           	//     if (arr[i] == 0)
+           	//     {
+           	//         return false;
+           	//     }
+           	// }
+           	// return true;
+
+           	// seen variable
+            int seen = 0;
+            for (auto i: sentence)
             {
-                return false;
+                int word = i - 'a';
+
+               	// shifting left the bit by value = word    ex 25 incase of z
+                int currBit = 1 << word;
+
+                seen = seen | currBit;
             }
-            int arr[26] = { 0 };
-            for (int i = 0; i < sentence.length(); i++)
-            {
-                int word = sentence[i] - 'a';
-                arr[word]++;
-            }
-            for (int i = 0; i < 26; i++)
-            {
-                if (arr[i] == 0)
-                {
-                    return false;
-                }
-            }
-            return true;
+            return seen == (1 << 26) - 1;
         }
 };
