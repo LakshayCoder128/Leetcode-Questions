273aced79edd4dacaf566b827749a5b544e42208
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-02-14 11:28:40 +0530

    Time: 14 ms (50.83%), Space: 7.8 MB (18.29%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,13 +1,35 @@
-class Solution {
-public:
-    bool isAnagram(string s, string t) {
-        if(s.length() != t.length()) return false;
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        
-        for(int i=0;i<s.length();i++){
-            if(s[i] != t[i])return false;
+class Solution
+{
+    public:
+        bool isAnagram(string s, string t)
+        {
+            /*
+            T.C. -> O(2*nlogn)   S.C. -> 2*logn
+            if(s.length() != t.length()) return false;
+            sort(s.begin(),s.end());
+            sort(t.begin(),t.end());
+
+            for(int i=0;i < s.length();i++){
+                if(s[i] != t[i])return false;
+            }
+            return true;
+            */
+
+           	// map  T.C. O(2*n) + SC = O(2*n)
+            if (s.length() != t.length()) return false;
+            unordered_map<char, int> sm;
+            unordered_map<char, int> tm;
+
+            for (int i = 0; i < s.length(); i++)
+            {
+                sm[s[i]]++;
+                tm[t[i]]++;
+            }
+
+            for (int i = 0; i < sm.size(); i++)
+            {
+                if (sm[i] != tm[i]) return false;
+            }
+            return true;
         }
-        return true;
-    }
 };
