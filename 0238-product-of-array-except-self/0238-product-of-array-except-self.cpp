6bb24582ad0ef92a19d8677c82ac075b338e29fb
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-03-29 21:32:22 +0530

    Time: 28 ms (51.93%), Space: 23.9 MB (90.54%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -36,21 +36,39 @@ class Solution
             
             
             // O(n+n)
-            vector<int>prefix(nums.size(),1);
-            vector<int>postfix(nums.size(),1);
+//             vector<int>prefix(nums.size(),1);
+//             vector<int>postfix(nums.size(),1);
             
-            prefix[0] = nums[0];
-            postfix[nums.size()-1] = nums[nums.size()-1];
-            for(int i=1;i<nums.size();i++) prefix[i] = prefix[i-1]*nums[i];
+//             prefix[0] = nums[0];
+//             postfix[nums.size()-1] = nums[nums.size()-1];
+//             for(int i=1;i<nums.size();i++) prefix[i] = prefix[i-1]*nums[i];
             
-            for(int i=nums.size()-2;i>=0;i--){
-                postfix[i] = postfix[i+1]*nums[i];
+//             for(int i=nums.size()-2;i>=0;i--){
+//                 postfix[i] = postfix[i+1]*nums[i];
+//             }
+//             nums[0] = postfix[1];
+//             nums[nums.size()-1] = prefix[nums.size()-2];
+//             for(int i=1;i<nums.size()-1;i++){
+//                 nums[i] = postfix[i+1] * prefix[i-1];
+//             }
+//             return nums;
+            
+            
+//             O(n+1) -> only space for answer returning
+            vector<int>res(nums.size(),1);
+            int prefix = 1;
+            for(int i=0;i<nums.size();i++){
+                res[i] = prefix;
+                prefix *= nums[i];
             }
-            nums[0] = postfix[1];
-            nums[nums.size()-1] = prefix[nums.size()-2];
-            for(int i=1;i<nums.size()-1;i++){
-                nums[i] = postfix[i+1] * prefix[i-1];
+            int postfix = 1;
+            for(int i=nums.size()-1;i>=0;i--){
+                res[i] = postfix*res[i];
+                postfix*=nums[i];
             }
-            return nums;
+            return res;
         }
 };
+
+// 1 2 3 4 
+// 1 2 6 
