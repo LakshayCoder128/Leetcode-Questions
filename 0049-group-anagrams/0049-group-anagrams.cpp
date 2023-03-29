c6aa6cf3277404425958383c8309b4edcff784e8
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-03-29 19:17:12 +0530

    Time: 134 ms (10.17%), Space: 27.9 MB (9.49%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,31 +3,26 @@ class Solution
     public:
         vector<vector < string>> groupAnagrams(vector<string> &strs)
         {
-            unordered_map<string, vector < string>> mp;
-            for (string s: strs)
+            unordered_map<string, vector < string>> m;
+            for (auto i: strs)
             {
-                mp[strSort(s)].push_back(s);
+                vector<int> count(26);
+                for (auto j:i)
+                {
+                    count[j - 'a']++;
+                }
+                string key = "";
+                for (int i = 0; i < 26; i++)
+                {
+                    key.append(to_string(count[i] + 'a'));
+                }
+                m[key].push_back(i);
             }
-            vector<vector < string>> anagrams;
-            for (auto p: mp)
+            vector<vector < string>> result;
+            for (auto it = m.begin(); it != m.end(); it++)
             {
-                anagrams.push_back(p.second);
+                result.push_back(it->second);
             }
-            return anagrams;
-        }
-    private:
-        string strSort(string s)
-        {
-            int counter[26] = { 0 };
-            for (char c: s)
-            {
-                counter[c - 'a']++;
-            }
-            string t;
-            for (int c = 0; c < 26; c++)
-            {
-                t += string(counter[c], c + 'a');
-            }
-            return t;
+            return result;
         }
 };
