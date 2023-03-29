416281c385087452d901954a65fddf4a0d755dcb
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-03-29 19:25:40 +0530

    Time: 31 ms (89.58%), Space: 20.8 MB (33.93%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    
+    // O(m*nlogn + m*n)
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         // map 
         unordered_map<string, vector<string>> mp;
@@ -24,7 +26,9 @@ public:
 //     vector<vector<string>> groupAnagrams(vector<string>& strs) {
 //         unordered_map<string, vector<string>> m;
 //         for (int i = 0; i < strs.size(); i++) {
+//             key generate ki
 //             string key = getKey(strs[i]);
+//             us key ko map kiya 
 //             m[key].push_back(strs[i]);
 //         }
         
@@ -36,13 +40,14 @@ public:
 //     }
 // private:
 //     string getKey(string str) {
-//         vector<int> count(26);
+//         vector<int> count(26); // freuency store
 //         for (int j = 0; j < str.size(); j++) {
 //             count[str[j] - 'a']++;
 //         }
         
 //         string key = "";
 //         for (int i = 0; i < 26; i++) {
+//              key sorted order me generate ho gyi
 //             key.append(to_string(count[i] + 'a'));
 //         }
 //         return key;
