40aada7b78ee8efa7460ca6d67e50493efea0f36
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-03-29 17:25:48 +0530

    Time: 8 ms (94.08%), Space: 10.9 MB (27.77%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,63 +3,19 @@ class Solution
     public:
         vector<int> twoSum(vector<int> &nums, int target)
         {
-           	//         O(N^2)
-           	// vector<int> ans;
-           	// for(int i=0;i < nums.size()-1;i++){
-           	//     for(int j=i+1;j < nums.size();j++){
-           	//         if(nums[i]+nums[j]==target){
-           	//             ans.push_back(i);
-           	//             ans.push_back(j);
-           	//         }
-           	//     }
-           	// }
-           	// return ans;
-
-           	//   O(N)  Auxiliary space : O(N)
-
-           	// vector<int> v;
-           	// unordered_map<int, int> m;
-           	// for (int i = 0; i < nums.size(); i++)
-           	// {
-           	//     if (m.find(target - nums[i]) != m.end())
-           	//     {
-           	//         v.push_back(m[target - nums[i]]);
-           	//         v.push_back(i);
-           	//         return v;
-           	//     }
-           	//     else
-           	//         m[nums[i]] = i;
-           	// }
-           	// return v;
-            /*
-            Example 
-            2,7,11,15      target =9
-
-            9-2=7
-            m.find(7)!=m.end()  	// false 
-            m[2]=0
-
-            m.find(9-7=2)!=m.end()  	// false
-            m[7]=1;
-
-            m.find(9-11=-2) 
-            m[-2]
-
-            */
-
-            vector<int> ans;
             unordered_map<int, int> m;
 
             for (int i = 0; i < nums.size(); i++)
             {
                 if (m.find(target - nums[i]) != m.end())
                 {
-                    ans.push_back(m[target - nums[i]]);
-                    ans.push_back(i);
-                    return ans;
+                    return {i,m[target - nums[i]]};
                 }
-                m[nums[i]] = i;
+                else
+                {
+                    m[nums[i]] = i;
+                }
             }
-            return ans;
+            return { -1,-1 };
         }
 };
