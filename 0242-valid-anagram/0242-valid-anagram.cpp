7038dfd68626039715ad34c76bbaeca6c5f5dea4
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-03-29 17:05:05 +0530

    Time: 11 ms (58.14%), Space: 7.5 MB (17.00%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,32 +3,17 @@ class Solution
     public:
         bool isAnagram(string s, string t)
         {
-            /*
-            T.C. -> O(2*nlogn)   S.C. -> 2*logn
-            if(s.length() != t.length()) return false;
-            sort(s.begin(),s.end());
-            sort(t.begin(),t.end());
-
-            for(int i=0;i < s.length();i++){
-                if(s[i] != t[i])return false;
-            }
-            return true;
-            */
-
-           	// map  T.C. O(2*n) + SC = O(2*n)
             if (s.length() != t.length()) return false;
-            unordered_map<char, int> sm;
-            unordered_map<char, int> tm;
+            unordered_map<char, int> m;
 
-            for (int i = 0; i < s.length(); i++)
+            for (auto i: s)
             {
-                sm[s[i]]++;
-                tm[t[i]]++;
+                m[i]++;
             }
-
-            for (int i = 0; i < sm.size(); i++)
+            for (auto i: t)
             {
-                if (sm[i] != tm[i]) return false;
+                m[i]--;
+                if (m[i] < 0) return false;
             }
             return true;
         }
