47a4b02ed50bfa3e80ab86a122d873e299b76b75
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-03-29 19:21:12 +0530

    Time: 129 ms (11.21%), Space: 27.9 MB (10.64%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,46 +1,58 @@
+// class Solution
+// {
+//     public:
+//         vector<vector < string>> groupAnagrams(vector<string> &strs)
+//         {
+//             unordered_map<string, vector < string>> m;
+//             for (auto i: strs)
+//             {
+//                 vector<int> count(26);
+//                 for (auto j: i)
+//                 {
+//                     count[j - 'a']++;
+//                 }
+//                 string key = "";
+//                 for (int i = 0; i < 26; i++)
+//                 {
+//                     key.append(to_string(count[i] + 'a'));
+//                 }
+//                 m[key].push_back(i);
+//             }
+//             vector<vector < string>> result;
+//             for (auto it = m.begin(); it != m.end(); it++)
+//             {
+//                 result.push_back(it->second);
+//             }
+//             return result;
+//         }
+// };
+
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> mp;
-        for (string s : strs) {
-            string t = s; 
-            sort(t.begin(), t.end());
-            mp[t].push_back(s);
+        unordered_map<string, vector<string>> m;
+        for (int i = 0; i < strs.size(); i++) {
+            string key = getKey(strs[i]);
+            m[key].push_back(strs[i]);
         }
-        vector<vector<string>> anagrams;
-        for (auto p : mp) { 
-            anagrams.push_back(p.second);
+        
+        vector<vector<string>> result;
+        for (auto it = m.begin(); it != m.end(); it++) {
+            result.push_back(it->second);
         }
-        return anagrams;
+        return result;
+    }
+private:
+    string getKey(string str) {
+        vector<int> count(26);
+        for (int j = 0; j < str.size(); j++) {
+            count[str[j] - 'a']++;
+        }
+        
+        string key = "";
+        for (int i = 0; i < 26; i++) {
+            key.append(to_string(count[i] + 'a'));
+        }
+        return key;
     }
 };
-
-// class Solution {
-// public:
-//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-//         unordered_map<string, vector<string>> m;
-//         for (int i = 0; i < strs.size(); i++) {
-//             string key = getKey(strs[i]);
-//             m[key].push_back(strs[i]);
-//         }
-        
-//         vector<vector<string>> result;
-//         for (auto it = m.begin(); it != m.end(); it++) {
-//             result.push_back(it->second);
-//         }
-//         return result;
-//     }
-// private:
-//     string getKey(string str) {
-//         vector<int> count(26);
-//         for (int j = 0; j < str.size(); j++) {
-//             count[str[j] - 'a']++;
-//         }
-        
-//         string key = "";
-//         for (int i = 0; i < 26; i++) {
-//             key.append(to_string(count[i] + 'a'));
-//         }
-//         return key;
-//     }
-// };
