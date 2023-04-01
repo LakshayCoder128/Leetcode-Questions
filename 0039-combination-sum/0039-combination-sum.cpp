faed038fc52d2aa1f0b1fd06f034aa9d8f0d7220
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-04-01 10:44:28 +0530

    Time: 13 ms (37.37%), Space: 17 MB (29.80%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -5,7 +5,7 @@ class Solution
         {
            	// base case 
 
-            if (target < 0) return;
+            if (target < 0 || index >= candidates.size()) return;
             if (target == 0)
             {
                 ans.push_back(temp);
@@ -13,14 +13,23 @@ class Solution
             }
 
            	// processing and call
-            for (int i = index; i < candidates.size(); i++)
-            {
-                temp.push_back(candidates[i]);
-                solve(candidates, target - candidates[i], ans, i, temp);
-               	// back track
-                temp.pop_back();
-               	// solve(candidates,target,ans,i,temp);
-            }
+           	//             for (int i = index; i < candidates.size(); i++)
+           	//             {
+           	//                 temp.push_back(candidates[i]);
+           	//                 solve(candidates, target - candidates[i], ans, i, temp);
+           	//                	// back track
+           	//                 temp.pop_back();
+
+           	//             }
+
+           	// ANother type of call exclude include
+           	// exclude
+            solve(candidates, target, ans, index + 1, temp);
+
+            temp.push_back(candidates[index]);
+
+            solve(candidates, target - candidates[index], ans, index, temp);
+            temp.pop_back();
         }
 
     vector<vector < int>> combinationSum(vector<int> &candidates, int target)
