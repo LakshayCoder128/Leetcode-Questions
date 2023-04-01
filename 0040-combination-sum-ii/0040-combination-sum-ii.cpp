581e8d4f77e6b22bf580c44084b14de5996330a6
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-04-01 11:59:56 +0530

    Time: 7 ms (60.50%), Space: 10.7 MB (69.41%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,30 +1,3 @@
-// Partially accepted 172/176
-
-// class Solution {
-// public:
-//     void solve(vector<int>& candidates, int target,set<vector<int>>&ans,vector<int>&t,int ind){
-//         if(target < 0) return;
-//         if(target == 0){ans.insert(t);return;}
-
-// for(int i=ind;i < candidates.size();i++){
-//     t.push_back(candidates[i]);
-//     solve(candidates,target-candidates[i],ans,t,i+1);
-//     t.pop_back();
-// }
-//     }
-//     vector<vector < int>> combinationSum2(vector<int>& candidates, int target) {
-//         set<vector < int>>ans;
-//         vector < int>t;
-//         sort(candidates.begin(),candidates.end());
-//         solve(candidates,target,ans,t,0);
-//         vector<vector < int>>finans;
-//         for(auto i:ans){
-//             finans.push_back(i);
-//         }
-
-//         return finans;
-//     }
-// };
 class Solution
 {
     public:
@@ -45,9 +18,8 @@ class Solution
                	// candidates[i] == candidates[i-1]	// kyunko agr ye dono equal hai to is value ke liye hum phle hi traverse    
                	// kr chuke hai aur ye sab possible ho paya sirf sorting ki wjh se  
                	// jab i taken index se aage ho aur  currindex ki value taken index ke barabar ho to mt lena 
-                if (i > ind && candidates[i] == candidates[i - 1]) continue;
-
                 if (candidates[i] > target) break;
+                if (i > ind && candidates[i] == candidates[i - 1]) continue;
                 t.push_back(candidates[i]);
                 solve(candidates, target - candidates[i], ans, t, i + 1);
                 t.pop_back();
@@ -57,11 +29,8 @@ class Solution
     {
         vector<vector < int>> ans;
         vector<int> t;
-
         sort(candidates.begin(), candidates.end());
-
         solve(candidates, target, ans, t, 0);
-
         return ans;
     }
 };
