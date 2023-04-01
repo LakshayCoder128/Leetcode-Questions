ede2b9598610dc40c3f186549591bb35bfa39967
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-04-01 11:39:58 +0530

    Time: 3 ms (91.09%), Space: 10.7 MB (69.41%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,63 @@
+// Partially accepted 172/176
+
+// class Solution {
+// public:
+//     void solve(vector<int>& candidates, int target,set<vector<int>>&ans,vector<int>&t,int ind){
+//         if(target < 0) return;
+//         if(target == 0){ans.insert(t);return;}
+
+// for(int i=ind;i < candidates.size();i++){
+//     t.push_back(candidates[i]);
+//     solve(candidates,target-candidates[i],ans,t,i+1);
+//     t.pop_back();
+// }
+//     }
+//     vector<vector < int>> combinationSum2(vector<int>& candidates, int target) {
+//         set<vector < int>>ans;
+//         vector < int>t;
+//         sort(candidates.begin(),candidates.end());
+//         solve(candidates,target,ans,t,0);
+//         vector<vector < int>>finans;
+//         for(auto i:ans){
+//             finans.push_back(i);
+//         }
+
+//         return finans;
+//     }
+// };
+class Solution
+{
+    public:
+        void solve(vector<int> &candidates, int target, vector<vector< int >> &ans, vector< int > &t, int ind)
+        {
+            if (target < 0) return;
+            if (target == 0)
+            {
+                ans.push_back(t);
+                return;
+            }
+
+            for (int i = ind; i < candidates.size(); i++)
+            {
+                
+                // special condition 
+                // jab i taken index se aage ho aur  currindex ki value taken index ke barabar ho to mt lena 
+                if(i > ind && candidates[i] == candidates[i-1]) continue;
+                
+                t.push_back(candidates[i]);
+                solve(candidates, target - candidates[i], ans, t, i + 1);
+                t.pop_back();
+            }
+        }
+    vector<vector < int>> combinationSum2(vector<int> &candidates, int target)
+    {
+        vector<vector < int>> ans;
+        vector<int> t;
+
+        sort(candidates.begin(), candidates.end());
+
+        solve(candidates, target, ans, t, 0);
+
+        return ans;
+    }
+};
