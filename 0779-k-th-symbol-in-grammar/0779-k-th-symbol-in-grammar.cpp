c9ac39e4bdf0de83bd4bdd5b330497fcd41df7b5
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-04-15 14:16:48 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (34.20%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -1,25 +1,15 @@
-class Solution {
-public:
-    void solve(int n,string &a,int i){
-        if(n==1) return ;
-        // processing
-        string t = "";
-        for(int i=0;i<a.length();i++){
-            if(a[i] == '0') t+="01";
-            else if(a[i] == '1') t+="10";
-        }
-        a = t;
-        solve(n-1,a,0);
-    }
-    int kthGrammar(int n, int k) {
-        // string a = "0";
-        // solve(n,a,0);
-        // cout<<n<<" "<<a<<endl;
-        // return a[k-1] == '0' ? 0:1;
+class Solution
+{
+    public:
+       	
+        int kthGrammar(int n, int k)
+        {
+            if (n == 1 || k == 1) return 0;
+            int mid = pow(2, n - 2);	// mid element :  length of nth row  =  2^n-1   so mid = 2^n-2
 
-        if(n==1 || k == 1) return 0;
-        int mid = pow(2,n-2);
-        if(k<=mid) return kthGrammar(n-1,k);
-        else return !kthGrammar(n-1,k-mid);
-    }
+           	// if k is less than mid then we can simply check k for n-1 row as the string before mid in nth row is the copy of the n-1 th row 
+            if (k <= mid) return kthGrammar(n - 1, k);
+           	// otherwise we obsereved that the string after the mid is complement of the n-1 th string 
+            else return !kthGrammar(n - 1, k - mid);
+        }
 };
