9a1f07f1d2427952a14e28d6f7f4eb37ca078fdb
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-05-22 11:46:55 +0530

    Time: 21 ms (32.68%), Space: 13.8 MB (24.64%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,51 +1,22 @@
-// class Solution
-// {
-//     public:
-// O(nlogn + n)
-//         vector<int> topKFrequent(vector<int> &nums, int k)
-//         {
-//             map<int, int> m;
-//             for (auto i: nums) m[i]++;
-//             priority_queue<pair<int, int>> pq;
-
-//             for (auto i: m)
-//             {
-//                 pq.push({ i.second,
-//                     i.first });
-//             }
-//             vector<int> ans;
-//             for (int i = 0; i < k; i++)
-//             {
-//                 ans.push_back(pq.top().second);
-//                 pq.pop();
-//             }
-//             return ans;
-//         }
-// };
-
-class Solution
-{
-    public:
-    // O(n+n)
-        vector<int> topKFrequent(vector<int> &nums, int k)
-        {
-            unordered_map<int, int> freq;
-            for (auto num: nums) freq[num]++;
-
-            vector<vector < int>> buckets(nums.size() + 1);
-            for (auto[a, b]: freq)
-                buckets[b].push_back(a);
-
-            vector<int> res;
-            for (int i = nums.size(); k; i--)
-            {
-                for (auto a: buckets[i])
-                {
-                    res.push_back(a);
-                    k--;
-                }
-            }
-
-            return res;
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int,int>m;
+        vector<int>ans;
+        for(auto i:nums){
+            m[i]++;
         }
+
+        priority_queue<pair<int,int>>pq;
+        for(auto i:m){
+            pq.push({i.second,i.first});
+        }
+
+        for(int i=0;i<k;i++){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+
+    }
 };
