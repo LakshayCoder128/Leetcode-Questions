53d273076b0b8ddad36d0de82e60ece80bac3f7a
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-05-23 09:43:54 +0530

    Time: 20 ms (74.98%), Space: 6.9 MB (90.02%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        
+        int start = 0;
+        int resLen = 0;
+        for(int i=0;i<s.length();i++){
+            // odd
+            int left = i,right = i;
+            while(left >=0 && right < s.length() && s[left] == s[right]){
+                if(right - left +1 > resLen) {start = left;  resLen = right - left +1;}
+                left --;
+                right++;
+            }
+            
+            // evenlen
+            left = i,right = i+1;
+            while(left >=0 && right < s.length() && s[left] == s[right]){
+                if(right - left +1 > resLen) {start = left;  resLen = right - left +1;}
+                left --;
+                right++;
+            }
+            
+        }
+        return s.substr(start , resLen);
+        // Dp solution O(n²) + O(n²)
+        /*
+        int size = s.size();
+        vector<vector<bool>>dp(size,vector<bool>(size));
+        int start = 0,end = 0;
+        int maxLen = 1;
+        for(int col = 0;col <size ; col++){
+            // for diagonal movement 
+            for(int row = 0, cl = col ;cl < size ;row++,cl++){
+                
+                // if length == 1 
+                if(col == 0) dp[row][cl] = true;
+                // if length == 2
+                else if (col == 1) {
+                    if(s[row] == s[cl]) {
+                        dp[row][cl] = true; 
+                        maxLen = 2;
+                        start = row;
+                    }
+                    else dp[row][cl] = false;
+                }
+                // other cases
+                else {
+                    if(s[row] == s[cl] && dp[row+1][cl -1] == true) {
+                        dp[row][cl] = true;
+                    }
+                    else dp[row][cl] = false;
+                }
+                if(dp[row][cl]){
+                    maxLen < col+1 ? maxLen = col+1,start = row :maxLen;
+                    
+                }
+            }
+        }
+        return s.substr(start,maxLen);
+        */
+    }
+};
