d429f4b8e5829da109cd0f2a04be91678f5c3fd1
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-06-01 21:11:27 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.cpp
+++ Coin Change - GFG/coin-change.cpp
@@ -28,11 +28,29 @@ class Solution {
       
       return dp[ind][sum] = inc + exc;
   }
+  long long int Tabsolve(int coins[],int N,int S){
+      
+      vector<vector<long long int>>dp(N+1,vector<long long int>(S+1,0));
+      
+      for(int i=0;i<N;i++){
+          dp[i][0] = 1;
+      }
+      
+      for(int ind =0 ; ind < N ;ind++){
+          for(int sum = 1 ; sum <= S;sum++){
+                long long int exc = (ind >= 1 ) ? dp[ind-1][sum] : 0;
+                long long int inc = (sum - coins[ind] >= 0) ? dp[ind][sum - coins[ind]] : 0;
+                dp[ind][sum] = inc + exc;
+          }
+      }
+      return dp[N-1][S];
+  }
+  
     long long int count(int coins[], int N, int sum) {
         int ind = 0;
         vector<vector<long long int>>dp(N+1,vector<long long int>(sum+1,-1));
         // int ans = Recsolve(coins,N,sum,ind);
-        long long int ans = RecMemosolve(coins,N,sum,ind,dp);
+        long long int ans = Tabsolve(coins,N,sum);
         return ans;
     }
 };
