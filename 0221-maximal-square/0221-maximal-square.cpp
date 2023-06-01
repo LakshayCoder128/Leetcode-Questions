b0efdce26f45035d0282d76d501f8721da309e87
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-06-01 18:40:33 +0530

    Time: 96 ms (58.24%), Space: 21 MB (44.35%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,47 +1,76 @@
 class Solution
 {
     public:
-//         int solve(vector<vector < char>> &matrix, int i, int j, int &maximumSide)
+       	//         int solve(vector<vector < char>> &matrix, int i, int j, int &maximumSide)
+       	//         {
+       	//             if (i >= matrix.size() || j >= matrix[0].size()) return 0;
+
+       	//             int down = solve(matrix, i + 1, j, maximumSide);
+       	//             int right = solve(matrix, i, j + 1, maximumSide);
+       	//             int diagonal = solve(matrix, i + 1, j + 1, maximumSide);
+
+       	//             if (matrix[i][j] == '1')
+       	//             {
+       	//                 int side = (min(down, min(right, diagonal))) + 1;
+       	//                 maximumSide = max(maximumSide, side);
+       	//                 return side;
+       	//             }
+       	//             else return 0;
+       	//         }
+
+       	// Recursion + memo
+//         int Dpsolve(vector<vector < char>> &matrix, int i, int j, int &maximumSide, vector< vector< int>> &dp)
 //         {
 //             if (i >= matrix.size() || j >= matrix[0].size()) return 0;
 
-//             int down = solve(matrix, i + 1, j, maximumSide);
-//             int right = solve(matrix, i, j + 1, maximumSide);
-//             int diagonal = solve(matrix, i + 1, j + 1, maximumSide);
+//             if (dp[i][j] != -1) return dp[i][j];
+
+//             int down = Dpsolve(matrix, i + 1, j, maximumSide, dp);
+//             int right = Dpsolve(matrix, i, j + 1, maximumSide, dp);
+//             int diagonal = Dpsolve(matrix, i + 1, j + 1, maximumSide, dp);
 
 //             if (matrix[i][j] == '1')
 //             {
 //                 int side = (min(down, min(right, diagonal))) + 1;
 //                 maximumSide = max(maximumSide, side);
-//                 return side;
+//                 return dp[i][j] = side;
 //             }
-//             else return 0;
+//             else return dp[i][j] = 0;
 //         }
 
-    int Dpsolve(vector<vector < char>> &matrix, int i, int j, int &maximumSide, vector< vector< int>> &dp)
+    int Tabsolve(vector<vector < char>> &matrix,int maximumSide)
     {
-        if (i >= matrix.size() || j >= matrix[0].size()) return 0;
-
-        if (dp[i][j] != -1) return dp[i][j];
         
-        int down = Dpsolve(matrix, i + 1, j, maximumSide,dp);
-        int right = Dpsolve(matrix, i, j + 1, maximumSide,dp);
-        int diagonal = Dpsolve(matrix, i + 1, j + 1, maximumSide,dp);
-
-        if (matrix[i][j] == '1')
+        vector<vector < int>> dp(matrix.size()+1, vector<int> (matrix[0].size()+1, 0));
+        
+        
+        for (int i = matrix.size() - 1; i >= 0; i--)
         {
-            int side = (min(down, min(right, diagonal))) + 1;
-            maximumSide = max(maximumSide, side);
-            return dp[i][j] = side;
+            for (int j = matrix[0].size() - 1; j >= 0; j--)
+            {
+                
+                int down = dp[i + 1][j];
+                int right = dp[i][j + 1];
+                int diagonal = dp[i + 1][j + 1];
+
+                if (matrix[i][j] == '1')
+                {
+                    int side = (min(down, min(right, diagonal))) + 1;
+                    maximumSide = max(maximumSide, side);
+                    dp[i][j] = side;
+                }
+            
+            }
         }
-        else return dp[i][j] = 0;
+        return maximumSide;
     }
     int maximalSquare(vector<vector < char>> &matrix)
     {
-        vector<vector < int>> dp(matrix.size(), vector<int> (matrix[0].size(), -1));
+        // vector<vector < int>> dp(matrix.size(), vector<int> (matrix[0].size(), -1));
         int maximumSide = 0;
        	// Rsolve(matrix, 0, 0, maximumSide);
-        Dpsolve(matrix, 0, 0, maximumSide, dp);
+        //Dpsolve(matrix, 0, 0, maximumSide, dp);
+        maximumSide = Tabsolve(matrix,maximumSide);
 
         return maximumSide * maximumSide;
     }
