5ba617bf21ba8f70c258453dda59c44b3ede0d76
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-06-02 13:54:10 +0530

    Time: 374 ms (5.71%), Space: 94.9 MB (5.00%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,28 +1,64 @@
-class Solution {
-public:
-    int coinsU = INT_MAX;
-    int solve(vector<int>& coins, int amount,int ind,vector<vector<int>>&dp){
-        if(amount == 0 ) return dp[amount][ind] = 0;
-        if(ind >= coins.size()) return dp[amount][ind] = INT_MAX -1;
-        if(dp[amount][ind]!= -1) return dp[amount][ind];
-        int res = INT_MAX-1;
-        if(amount - coins[ind] < 0 )
+class Solution
+{
+    public:
+
+        int solve(vector<int> &coins, int amount, int ind, vector<vector< int>> &dp)
         {
-            int notChosen = 0 + solve(coins,amount,ind+1,dp);
-            res = notChosen;
+            if (amount == 0) return dp[amount][ind] = 0;
+            if (ind >= coins.size()) return dp[amount][ind] = INT_MAX - 1;
+            if (dp[amount][ind] != -1) return dp[amount][ind];
+            int res = INT_MAX - 1;
+            if (amount - coins[ind] < 0)
+            {
+                int notChosen = 0 + solve(coins, amount, ind + 1, dp);
+                res = notChosen;
+            }
+            else
+            {
+                int chosen = 1 + solve(coins, amount - coins[ind], ind, dp);
+                int notChosen = 0 + solve(coins, amount, ind + 1, dp);
+                res = min(notChosen, chosen);
+            }
+
+            return dp[amount][ind] = res;
         }
-        else {
-            int chosen = 1+solve(coins,amount-coins[ind],ind,dp);
-            int notChosen = 0 + solve(coins,amount,ind+1,dp);
-            res = min (notChosen,chosen);
+    int Tab(vector<int> coins, int amt)
+    {
+        vector<vector < int>> dp(amt + 1, vector<int> (coins.size() + 1, INT_MAX-1 ));
+
+        for (int amount = 0; amount <= amt; amount++)
+        {
+            for (int ind = coins.size() - 1; ind >= 0; ind--)
+            {
+                if (amount == 0) dp[amount][ind] = 0;
+                else
+                {
+                    int res = INT_MAX - 1;
+                    if (amount - coins[ind] < 0)
+                    {
+                        int notChosen = 0 + dp[amount][ind + 1];
+                        res = notChosen;
+                    }
+                    else
+                    {
+                        int chosen = 1 + dp[amount - coins[ind]][ind];
+                        int notChosen = 0 + dp[amount][ind + 1];
+                        res = min(notChosen, chosen);
+                    }
+
+                    dp[amount][ind] = res;
+                    // cout << dp[amount][res] << " ";
+                }
+            }
+            // cout << endl;
         }
-        
-        return dp[amount][ind] = res;
+        return dp[amt][0];
     }
-    int coinChange(vector<int>& coins, int amount) {
+    int coinChange(vector<int> &coins, int amount)
+    {
         int ind = 0;
-        vector<vector<int>>dp(amount+1,vector<int>(coins.size()+1,-1));
-        int ans = solve(coins,amount,ind,dp);
-        return  dp[amount][ind] == INT_MAX-1 ? -1 : dp[amount][ind];
+       	// vector<vector < int>> dp(amount + 1, vector<int> (coins.size() + 1, -1));
+        int ans = Tab(coins, amount);
+        return ans == INT_MAX - 1 ? -1 : ans;
     }
 };
