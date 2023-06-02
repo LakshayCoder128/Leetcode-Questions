d0b4aa6b8f23102f6ab9fa8e108fbb2784640ca9
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-06-02 09:42:32 +0530

    Time: 61 ms (65.08%), Space: 6.5 MB (96.47%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -48,9 +48,30 @@ class Solution
         }
         return dp[0][0];
     }
+
+    int spaceOptimized(string t1, string t2)
+    {
+        vector<int> curr(t2.length() + 1, 0);
+        vector<int> next(t2.length() + 1, 0);
+        for (int i = t1.size() - 1; i >= 0; i--)
+        {
+            for (int j = t2.size() - 1; j >= 0; j--)
+            {
+                if (t1[i] == t2[j]) curr[j] = 1 + next[j + 1];
+                else
+                {
+                    int op1 = next[j];
+                    int op2 = curr[j + 1];
+                    curr[j] = max(op1, op2);
+                }
+            }
+            next = curr;
+        }
+        return next[0];
+    }
     int longestCommonSubsequence(string text1, string text2)
     {
        	// vector<vector < int>> dp(text1.size() + 1, vector<int> (text2.size(), -1));
-        return tabSolve(text1, text2);
+        return spaceOptimized(text1, text2);
     }
 };
