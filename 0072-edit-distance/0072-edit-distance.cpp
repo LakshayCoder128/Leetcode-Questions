2f15cd4f7541a40b61e529d06f57705dfb8d8c5d
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-06-02 11:05:16 +0530

    Time: 17 ms (53.17%), Space: 11.6 MB (17.59%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -19,29 +19,60 @@ class Solution
                     subs }) + 1;
             }
         }
-    int Msolve(string &w1, string &w2, int i, int j,vector<vector<int>>&dp)
+    int Msolve(string &w1, string &w2, int i, int j, vector<vector < int>> &dp)
+    {
+        if (i >= w1.length()) return w2.length() - j;
+        if (j >= w2.length()) return w1.length() - i;
+
+        if (dp[i][j] != -1) return dp[i][j];
+        if (w1[i] == w2[j]) return dp[i][j] = Msolve(w1, w2, i + 1, j + 1, dp);
+        else
         {
-            if (i >= w1.length()) return w2.length() - j;
-            if (j >= w2.length()) return w1.length() - i;
 
-            if(dp[i][j] != -1)return dp[i][j];
-            if (w1[i] == w2[j]) return dp[i][j] = Msolve(w1, w2, i + 1, j + 1,dp);
-            else
+            int insertion = Msolve(w1, w2, i, j + 1, dp);
+            int deletion = Msolve(w1, w2, i + 1, j, dp);
+            int subs = Msolve(w1, w2, i + 1, j + 1, dp);
+
+            return dp[i][j] = min({ insertion,
+                deletion,
+                subs }) + 1;
+        }
+    }
+    int TabS(string &w1, string &w2)
+    {
+        vector<vector < int>> dp(w1.size() + 1, vector<int> (w2.size() + 1, 0));
+        for (int i = 0; i < w1.length(); i++)
+        {
+            dp[i][w2.length()] = w1.length() - i;
+        }
+        for (int j = 0; j < w2.length(); j++)
+        {
+            dp[w1.length()][j] = w2.length() - j;
+        }
+
+        for (int i = w1.length() - 1; i >= 0; i--)
+        {
+            for (int j = w2.length() - 1; j >= 0; j--)
             {
+                if (w1[i] == w2[j]) dp[i][j] = dp[i + 1][j + 1];
+                else
+                {
 
-                int insertion = Msolve(w1, w2, i, j + 1,dp);
-                int deletion = Msolve(w1, w2, i + 1, j,dp);
-                int subs = Msolve(w1, w2, i + 1, j + 1,dp);
+                    int insertion = dp[i][j + 1];
+                    int deletion = dp[i + 1][j];
+                    int subs = dp[i + 1][j + 1];
 
-                return dp[i][j] = min({ insertion,
-                    deletion,
-                    subs }) + 1;
+                    dp[i][j] = min({ insertion,
+                        deletion,
+                        subs }) + 1;
+                }
             }
         }
-    
+        return dp[0][0];
+    }
     int minDistance(string word1, string word2)
     {
-        vector<vector<int>>dp(word1.size()+1,vector<int>(word2.size()+1,-1));
-        return Msolve(word1, word2, 0, 0,dp);
+        vector<vector < int>> dp(word1.size() + 1, vector<int> (word2.size() + 1, -1));
+        return TabS(word1, word2);
     }
 };
