5dfef127fa9c12ca233f3af0adf9763d061b7f8e
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-06-04 13:54:21 +0530

    Time: 5 ms (37.11%), Space: 6.6 MB (57.64%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+    public:
+        int Dsolve(vector<int> &nums, int target, vector<int> &dp)
+        {
+            if (target < 0) return 0;
+            if (target == 0) return 1;
+            if (dp[target] != -1) return dp[target];
+            int ways = 0;
+            for (int i = 0; i < nums.size(); i++)
+            {
+                ways += Dsolve(nums, target - nums[i], dp);
+            }
+            return dp[target] = ways;
+        }
+    int tabsolve(vector<int> &nums, int T)
+    {
+        vector<unsigned int> dp(T + 1, 1);
+        for (int target = 1; target <= T; target++)
+        {
+            int ways = 0;
+            for (int i = 0; i < nums.size(); i++)
+            {
+                ways += target - nums[i] >= 0 ? dp[target - nums[i]] : 0;
+            }
+            dp[target] = ways;
+        }
+        return dp[T];
+    }
+    int combinationSum4(vector<int> &nums, int target)
+    {
+        // vector<int> Dp(target + 1, -1);
+        // int a = Dsolve(nums, target, Dp);
+        int a = tabsolve(nums, target);
+        return a;
+    }
+};
