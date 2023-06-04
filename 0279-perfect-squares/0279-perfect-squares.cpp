e0c5dd7d6450836cb7c6320bd954cb6104791691
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-06-04 14:12:43 +0530

    Time: 281 ms (35.45%), Space: 9.6 MB (35.07%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,55 +1,24 @@
 class Solution
 {
     public:
-       	// recursive 
-        int solve(int num, int n)
+        int solve(int n,vector<int>&dp)
         {
             if (n == 0) return 0;
-            if (n - num * num < 0) return INT_MAX - 1;
+            if (n < 0) return INT_MAX - 2;
+            if(dp[n] != -1)return dp[n];
+            int ans = INT_MAX - 2;
 
-           	// exclu 
-            int exc = solve(num + 1, n);
-           	// incl
-            int incl = 1 + solve(num, n - num *num);
-
-            return min(exc, incl);
-        }
-
-   	// recursion  + memo 
-    int solvedp(int num, int n, vector<int> &dp)
-    {
-        if (n == 0) return 0;
-        if (n - num * num < 0) return INT_MAX - 1;
-        if (dp[n] != -1) return dp[n];
-       	// exclu 
-        int exc = solvedp(num + 1, n, dp);
-       	// incl
-        int incl = 1 + solvedp(num, n - num *num, dp);
-        return dp[n] = min(exc, incl);
-    }
-   	// bottomup
-    int solveBu(int N)
-    {
-        vector<int> dp(N + 1, 0);
-
-        for (int n = 1; n <= N; n++)
-        {
-
-            int ans = INT_MAX;
-
-            for (int i = 1; i*i <= n; i++)
+            for (int i = 1; i * i <= n; i++)
             {
-                ans = min(ans, 1 + dp[n - i *i]);
+                ans = min(ans, 1 + solve(n - i *i,dp));
             }
-            dp[n] = ans;
+
+            return dp[n] = ans;
         }
-        return dp[N];
-    }
     int numSquares(int n)
     {
-        int num = 1;
-        vector<int> dp(n + 1, -1);
-        int ans = solveBu(n);
-        return ans;
+        vector<int>Dp(n+1,-1);
+        int a = solve(n,Dp);
+        return a;
     }
 };
