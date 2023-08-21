9e0defecda9c81298a9041cb4505206835cbf798
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-08-21 13:01:13 +0530

    Time: 1081 ms (34.94%), Space: 9.5 MB (56.06%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -1,73 +1,25 @@
-// class Solution {
-// public:
-    
-// bool helper(vector<int>& nums,int index,int &req,int sum,int k) {
-//     if(k==0) {
-//         return true;
-//     }
-//     if(sum==req) {
-//         return helper(nums,0,req,0,k-1);
-//     }
-//     if(index>=nums.size()) {
-//         return false;
-//     }
-//     if(nums[index]!=-1 && sum+nums[index]<=req) {
-//         int n=nums[index];
-//         nums[index]=-1;
-//         if(helper(nums,index+1,req,sum+n,k)) {
-//             return true;
-//         }
-//         nums[index]=n;
-//         if(helper(nums,index+1,req,sum,k)) {
-//             return true;
-//         }
-//         return false;
-//     }
-//     else {
-//         return helper(nums,index+1,req,sum,k);
-//     }
-// }
-
-// bool canPartitionKSubsets(vector<int>& nums, int K) {
-//     int sum=0;
-//     for(int i=0;i<nums.size();i++) {
-//         sum+=nums[i];
-//     }
-//     if((sum%K)!=0) {
-//         return false;
-//     }
-//     int req=sum/K;
-//     return helper(nums,0,req,0,K);
-// }
-
-// };
 class Solution {
-public:
-    bool dfs(vector<int>& nums,int ind, int k,int sum,int &target,vector<bool> &vis)
-    {
-        if(k==1)    return true; //rest of all nums sum equal to target
-        if(ind==nums.size())    return false;
-        if(sum==target) return dfs(nums,0,k-1,0,target,vis);//imp base case if current subset sum equal target then move to next subset
-        bool pick=false;
-        if(!vis[ind] && sum+nums[ind]<=target)
-        {
-            vis[ind]=true;
-            pick=dfs(nums,ind+1,k,sum+nums[ind],target,vis);
-            if(pick)return true;//we can stop here it will reduce height of decission tree | to avoid TLE
-            vis[ind]=false;
-        }
-        bool npick=dfs(nums,ind+1,k,sum,target,vis);
-        if(pick||npick)    return true;
-        return false;
+    private:
+    bool solve(vector<int>& nums, int k,int i,int sum,int target,vector<int>&vis){
+        if(k == 1) return true;
+        if(i>=nums.size())return false;
+        if(sum == target) return solve(nums,k-1,0,0,target,vis);
+        bool take = 0;
+        if(sum + nums[i] <= target && vis[i] == 0){
+            vis[i] = 1 ;
+            take = solve(nums,k,i+1,sum+nums[i],target,vis);
+            if(take)return true;
+            vis[i] = 0;
+        } 
+        bool notTake = solve(nums,k,i+1,sum,target,vis);
+        return take | notTake;
     }
-    bool canPartitionKSubsets(vector<int>& nums, int k) 
-    {
-        int sum=0;
-        for(int i:nums)
-            sum+=i;
-        if(sum%k!=0 || k>sum)    return false;
-        sum/=k;
-        vector<bool> vis(nums.size(),false);
-        return dfs(nums,0,k,0,sum,vis);
+public:
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        int sum = 0;
+        for(auto &i:nums)sum+=i;  
+        if(sum%k != 0 || nums.size() < k)return false;
+        vector<int>vis(nums.size()+1,0);
+        return solve(nums,k,0,0,sum/k,vis);
     }
 };
