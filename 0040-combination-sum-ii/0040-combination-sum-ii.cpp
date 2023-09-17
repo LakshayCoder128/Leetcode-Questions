f1dcc21cf57841e08fda428027f86a9dd7d4d504
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-09-17 19:54:46 +0530

    Time: 3 ms (84.15%), Space: 11 MB (27.59%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,36 +1,37 @@
-class Solution
+class Solution {
+public:
+void solve(vector<int> &nums,int index,vector<int> &path,vector<vector<int>> &ans,int target)
 {
-    public:
-        void solve(vector<int> &candidates, int target, vector<vector< int >> &ans, vector< int > &t, int ind)
-        {
-            if (target < 0) return;
-            if (target == 0)
+      if(target==0)
+      {
+            ans.push_back(path);
+            return;
+      }
+      for(int i=index;i<nums.size();i++)
+      {
+            if(i!=index&&nums[i-1]==nums[i])
             {
-                ans.push_back(t);
-                return;
+                  continue;
             }
-
-            for (int i = ind; i < candidates.size(); i++)
+            if(target<nums[i])
             {
-
-               	// special condition 
-               	// is condition pr tabhi ayenge jb back track ho jaega kyunki usi ke baad (ind < i)  aur hume check kr rhe hai 
-               	// candidates[i] == candidates[i-1]	// kyunko agr ye dono equal hai to is value ke liye hum phle hi traverse    
-               	// kr chuke hai aur ye sab possible ho paya sirf sorting ki wjh se  
-               	// jab i taken index se aage ho aur  currindex ki value taken index ke barabar ho to mt lena 
-                if (candidates[i] > target) break;
-                if (i > ind && candidates[i] == candidates[i - 1]) continue;
-                t.push_back(candidates[i]);
-                solve(candidates, target - candidates[i], ans, t, i + 1);
-                t.pop_back();
+                  break;
             }
-        }
-    vector<vector < int>> combinationSum2(vector<int> &candidates, int target)
-    {
-        vector<vector < int>> ans;
-        vector<int> t;
-        sort(candidates.begin(), candidates.end());
-        solve(candidates, target, ans, t, 0);
-        return ans;
+            path.push_back(nums[i]);
+            solve(nums,i+1,path,ans,target-nums[i]);
+            path.pop_back();
+            
+      }
+
+
+}
+    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
+       
+     
+      sort(nums.begin(),nums.end());
+      vector<int> path;
+      vector<vector<int>> ans;
+      solve(nums,0,path,ans,target);
+      return ans;
     }
 };
