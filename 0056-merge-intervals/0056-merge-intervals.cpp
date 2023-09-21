a93603eb84f125e412e9b393a3b8ff78999ba116
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-09-21 18:42:59 +0530

    Time: 33 ms (39.38%), Space: 19.4 MB (33.28%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,35 +1,22 @@
-class Solution
-{
-    public:
-        vector<vector < int>> merge(vector<vector < int>> &intervals)
-        {
-            sort(intervals.begin(), intervals.end());
-
-           	//   for (auto i : intervals) {
-           	//     for (auto j : i) {
-           	//       cout << j << " ";
-           	//     }
-           	//     cout << endl;
-           	//   }
-            vector<vector < int>> ans;
-            int s = intervals[0][0];
-            int e = intervals[0][1];
-            for (int i = 1; i < intervals.size(); i++)
-            {
-                if (e >= intervals[i][0])
-                {
-                    e = max(e, intervals[i][1]);
-                }
-                else
-                {
-                    ans.push_back({ s,
-                        e });
-                    s = intervals[i][0];
-                    e = intervals[i][1];
-                }
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        int s = intervals[0][0];
+        int e = intervals[0][1];
+        vector<vector<int>>ans;
+        for(int i= 1;i<intervals.size();i++){
+            
+            if(e >= intervals[i][0]){
+                e = max(e,intervals[i][1]);
             }
-            ans.push_back({ s,
-                e });
-            return ans;
+            else{
+                ans.push_back({s,e});
+                s = intervals[i][0];
+                e = intervals[i][1];
+            }
         }
+        ans.push_back({s,e});
+        return ans;
+    }
 };
