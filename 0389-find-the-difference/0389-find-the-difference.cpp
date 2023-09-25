6296e32411b002a67abd81a97c6de9cca81e5dab
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-09-25 09:26:01 +0530

    Time: 0 ms (100%), Space: 7 MB (8.68%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+         char ans=0;
+        for(char i:s){
+            ans^=i;
+        }
+        for(char i:t){
+            ans^=i;
+        }
+        return ans;
+    }
+};
