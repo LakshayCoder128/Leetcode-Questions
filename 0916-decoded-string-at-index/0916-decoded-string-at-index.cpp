dc3d9dee8abab10eee2e9326f145c251d008c535
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-09-27 12:38:51 +0530

    Time: 0 ms (100%), Space: 6.4 MB (7.16%) - LeetHub

---
+++ 0916-decoded-string-at-index/0916-decoded-string-at-index.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+//     string decodeAtIndex(string s, int k) {
+//         if(k == 1){
+//             string a = "";a.push_back(s[0]);
+//             return a;
+//             }
+//         string ans = "";
+//         for(int i=0;i<s.length() && ans.length() < k+3;i++){
+//             if(isdigit(s[i])){
+//                 int t = s[i] - '0';
+//                 string rep = ans;
+//                 t--;
+//                 while(t--){
+//                     ans += rep;
+//                 }
+//             }
+//             else ans += s[i];
+//         }
+//         // cout<<ans<<" ";
+//         string a ;
+//         a.push_back(ans[k-1]);
+//         return a;
+//     }
+    string decodeAtIndex(string S, int K) {
+        long N = 0, i;
+        for (i = 0; N < K; ++i)
+            N = isdigit(S[i]) ? N * (S[i] - '0') : N + 1;
+        while (i--)
+            if (isdigit(S[i]))
+                N /= S[i] - '0', K %= N;
+            else if (K % N-- == 0)
+                return string(1, S[i]);
+        return "lee215";
+    }
+};
