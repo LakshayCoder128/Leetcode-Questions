84d05a3779b2a673df8f4fb92bcf72904d136a93
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-09-29 11:29:37 +0530

    Time: 118 ms (73.39%), Space: 96.7 MB (22.05%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.cpp
+++ 0932-monotonic-array/0932-monotonic-array.cpp
@@ -6,18 +6,23 @@ public:
         for(int i=1;i<nums.size();i++){
             if(nums[i-1] > nums[i]){
                 monoInc = false;
-                break;
+        
             }
-        }
-        if(monoInc == true)return true;
-        // monodec
-        for(int i=1;i<nums.size();i++){
+
             if(nums[i-1] < nums[i]){
                 monoDec = false;
-                break;
+                
             }
         }
-        return monoDec;
+        // if(monoInc == true)return true;
+        // // monodec
+        // for(int i=1;i<nums.size();i++){
+        //     if(nums[i-1] < nums[i]){
+        //         monoDec = false;
+        //         break;
+        //     }
+        // }
+        return monoDec || monoInc;
 
     }
 };
