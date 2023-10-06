9094b0212d9791ffafe99629319c8a9658179761
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-10-06 21:10:40 +0530

    Time: 3 ms (30.5%), Space: 6.6 MB (22.36%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int integerBreak(int n) {
-        return ((n>3)?pow(3,n/3-((n%3)&1))*pow(2,(n+n%3)%3):n-1);
+        vector<int> dp(n + 1, 0);
+        dp[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j < i; ++j) {
+                dp[i] = max(dp[i], max(j * (i - j), j * dp[i - j]));
+            }
+        }
+        return dp[n];
     }
 };
