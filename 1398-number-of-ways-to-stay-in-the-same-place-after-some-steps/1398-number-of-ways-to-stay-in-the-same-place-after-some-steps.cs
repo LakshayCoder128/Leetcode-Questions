1a55f3122008fe5071a67d3fa5c36d3042bc214c
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-10-15 13:49:54 +0530

    Time: 40 ms (92.86%), Space: 31.9 MB (92.86%) - LeetHub

--- 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/1398-number-of-ways-to-stay-in-the-same-place-after-some-steps.cs
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/1398-number-of-ways-to-stay-in-the-same-place-after-some-steps.cs
@@ -1,20 +1,25 @@
 public class Solution {
-    public int NumWays(int steps, int arrLen) {
-        const int MOD = 1000000007;
-        long[][] dp = new long[steps + 1][];
-        for (int i = 0; i <= steps; i++) {
-            dp[i] = new long[Math.Min(steps, arrLen) + 2];
-            for (int j = 0; j < Math.Min(steps, arrLen) + 2; j++) {
-                dp[i][j] = 0;
-            }
-        }
-        dp[0][0] = 1;
+    private const int MOD = 1000000007;
 
-        for (int step = 1; step <= steps; step++) {
-            for (int pos = 0; pos < Math.Min(steps, arrLen); pos++) {
-                dp[step][pos] = ((dp[step - 1][pos] + dp[step - 1][pos + 1]) % MOD + (pos > 0 ? dp[step - 1][pos - 1] : 0)) % MOD;
+    public int TopDown(int steps, int arrLen, int pos, int[][] dp) {
+        if (steps == 0 && pos == 0) return 1;
+        if (steps == 0 && pos != 0) return 0;
+        if (steps < 0 || pos < 0 || pos >= arrLen) return 0;
+        if (dp[steps][pos] != -1) return dp[steps][pos];
+        dp[steps][pos] = ((TopDown(steps - 1, arrLen, pos, dp) % MOD  // stay
+            + TopDown(steps - 1, arrLen, pos + 1, dp) % MOD) % MOD  // right
+            + TopDown(steps - 1, arrLen, pos - 1, dp) % MOD) % MOD;  // left
+        return dp[steps][pos];
+    }
+
+    public int NumWays(int steps, int arrLen) {
+        int[][] dp = new int[steps + 1][];
+        for (int i = 0; i <= steps; i++) {
+            dp[i] = new int[steps + 2]; 
+            for (int j = 0; j < steps + 2; j++) {
+                dp[i][j] = -1;
             }
         }
-        return (int)dp[steps][0];
+        return TopDown(steps, arrLen, 0, dp);
     }
 }
