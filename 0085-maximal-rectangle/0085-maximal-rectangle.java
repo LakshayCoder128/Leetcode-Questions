52af69f8eb36e9663479a2892c8f6f4f2d70858b
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-10-16 02:47:19 +0530

    Time: 13 ms (74.69%), Space: 44 MB (61.53%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+public class Solution {
+
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int n = heights.length;
+        for (int i = 0; i <= n; i++) {
+            while (!stack.isEmpty() && (i == n || heights[stack.peek()] >= heights[i])) {
+                int height = heights[stack.pop()];
+                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, width * height);
+            }
+            stack.push(i);
+        }
+        return maxArea;
+    }
+
+    public int maximalAreaOfSubMatrixOfAll1(char[][] matrix, int n, int m) {
+        int maxArea = 0;
+        int[] height = new int[m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == '1') {
+                    height[j]++;
+                } else {
+                    height[j] = 0;
+                }
+            }
+            int area = largestRectangleArea(height);
+            maxArea = Math.max(maxArea, area);
+        }
+        return maxArea;
+    }
+
+    public int maximalRectangle(char[][] matrix) {
+        return maximalAreaOfSubMatrixOfAll1(matrix, matrix.length, matrix[0].length);
+    }
+}
