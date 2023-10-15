b6f6dfb28e3c3e3d95f2c8966e7d39ff1a2774de
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-10-15 10:53:20 +0530

    Time: 7 ms (89.47%), Space: 6.6 MB (100%) - LeetHub

---
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/3150-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -0,0 +1,60 @@
+class Solution
+{
+    public:
+        string shortestBeautifulSubstring(string s, int k)
+        {
+            string ans = "";
+            int len = 0;
+            for (int i = 0; i < s.length(); i++)
+            {
+                int cnt = 0;
+                for (int j = i; j < s.length(); j++)
+                {
+                    if (s[j] == '1')
+                    {
+                        cnt++;
+                        if (cnt == k)
+                        {
+                            if (len == 0)
+                            {
+                                len = j - i + 1;
+                            }
+                            else if (len > j - i + 1)
+                            {
+                                len = j - i + 1;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < s.length(); i++)
+            {
+                int cnt = 0;
+                for (int j = i; j < s.length(); j++)
+                {
+                    if (s[j] == '1')
+                    {
+                        cnt++;
+                        if (cnt == k)
+                        {
+                            if (len == j - i + 1)
+                            {
+
+                                if (ans == "")
+                                {
+                                    ans = s.substr(i, len);
+                                }
+                                else
+                                {
+                                    ans = min(ans, s.substr(i, len));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return ans;
+        }
+};
