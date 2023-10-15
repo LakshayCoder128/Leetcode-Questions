c7dfa5d1fba4b38e72d179f94ec59b3a4978321d
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-10-15 13:56:58 +0530

    Time: 75 ms (78.95%), Space: 50.6 MB (63.16%) - LeetHub

--- 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/1398-number-of-ways-to-stay-in-the-same-place-after-some-steps.js
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/1398-number-of-ways-to-stay-in-the-same-place-after-some-steps.js
@@ -1,12 +1,20 @@
-var numWays = function (steps, arrLen) {
-    const MOD = 10 ** 9 + 7;
-    let dp = Array.from({ length: steps + 1 }, () => Array(Math.min(steps, arrLen) + 2).fill(0));
-    dp[0][0] = 1;
+var MOD = 1000000007;
 
-    for (let step = 1; step <= steps; step++) {
-        for (let pos = 0; pos < Math.min(steps, arrLen); pos++) {
-            dp[step][pos] = ((dp[step - 1][pos] + dp[step - 1][pos + 1]) % MOD + (pos > 0 ? dp[step - 1][pos - 1] : 0)) % MOD;
-        }
-    }
-    return dp[steps][0];
+var TopDown = function (steps, arrLen, pos, dp) {
+    if (steps === 0 && pos === 0) return 1;
+    if (steps === 0 && pos !== 0) return 0;
+    if (steps < 0 || pos < 0 || pos >= arrLen) return 0;
+    if (dp[steps][pos] !== -1) return dp[steps][pos];
+    dp[steps][pos] =
+        ((((TopDown(steps - 1, arrLen, pos, dp) % MOD) + // stay
+            (TopDown(steps - 1, arrLen, pos + 1, dp) % MOD)) %
+            MOD) + // right
+            (TopDown(steps - 1, arrLen, pos - 1, dp) % MOD)) %
+        MOD; // left
+    return dp[steps][pos];
+};
+
+var numWays = function (steps, arrLen) {
+    const dp = Array.from({ length: steps + 1 }, () => Array(steps+2).fill(-1));
+    return TopDown(steps, arrLen, 0, dp);
 };
