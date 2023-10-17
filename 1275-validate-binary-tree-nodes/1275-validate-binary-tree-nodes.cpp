dc798a0894ce647c09935af004c3b54d821510e2
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-10-17 11:08:30 +0530

    Time: 27 ms (95.9%), Space: 32.2 MB (96.2%) - LeetHub

--- 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.cpp
+++ 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.cpp
@@ -1,62 +1,50 @@
 class Solution {
 public:
-    // Helper function for Depth-First Search (DFS)
-    int dfs(vector<int> &left, vector<int> &right, int root) {
-        // If the current node is NULL, return 0 as there are no nodes in this subtree.
-        if (root == -1) {
-            return 0;
-        }
-        
-        // Recursively count the nodes in the left and right subtrees and add 1 for the current node.
-        return 1 + dfs(left, right, left[root]) + dfs(left, right, right[root]);
+    int component = -1;  // Initialize a component variable to keep track of the number of connected components.
+    
+    int findParent(vector<int>& parent, int x) {
+        while (parent[x] != x)
+            x = parent[parent[x]];
+        return x;
     }
-
+    
+    bool validChild(int child, int x, vector<int>& parent) {
+        if (parent[child] != child)  // If the child node already has a parent set, it has two parents, return false.
+            return false;
+        else {
+            int y = findParent(parent, child);
+            if (x != y) {
+                parent[y] = x;
+                component--;
+            } else {
+                return false;  // If (x == y), both belong to the same set, and there's a loop, return false.
+            }
+        }
+        return true;
+    }
+    
     bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
-        // Create a vector to keep track of the parent count for each node.
-        vector<int> parentCount(n, 0);
-
-        // Initialize the root node as -1 (no root yet).
+        vector<int> parent(n);
+        component = n;
         int root = -1;
 
-        // Iterate through the child nodes.
-        for (int i = 0; i < leftChild.size(); i++) {
-            // Check the left child.
-            if (leftChild[i] != -1) {
-                // If a node has more than one parent, return false.
-                if (parentCount[leftChild[i]]++ == 1) {
-                    return false;
-                }
-            }
-
-            // Check the right child.
-            if (rightChild[i] != -1) {
-                // If a node has more than one parent, return false.
-                if (parentCount[rightChild[i]]++ == 1) {
-                    return false;
-                }
-            }
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;  // Assign each node as its parent.
         }
 
-        // Find the root node and check for multiple roots.
-        for (int i = 0; i < leftChild.size(); i++) {
-            // If a node has no incoming edges (in-degree is 0) and has children, it's a candidate root.
-            if (parentCount[i] == 0) {
-                if (root == -1) {
-                    // If there is no previous candidate root, set the current node as the root.
-                    root = i;
-                } else {
-                    // If we already have a candidate root, return false (multiple roots).
+        for (int i = 0; i < n; i++) {
+            int x = findParent(parent, i);  // Find the parent of the current root node if either of its children is present; we have to perform the union later.
+
+            if (leftChild[i] != -1 || rightChild[i] != -1) {
+                if (leftChild[i] != -1 && !validChild(leftChild[i], x, parent)) {
+                    return false;
+                }
+                if (rightChild[i] != -1 && !validChild(rightChild[i], x, parent)) {
                     return false;
                 }
             }
         }
 
-        // If there is no root, return false (no valid tree).
-        if (root == -1) {
-            return false;
-        }
-
-        // Perform a depth-first search (DFS) from the root and check if all nodes are visited.
-        return dfs(leftChild, rightChild, root) == n;
+        return component == 1;  // After all unions, there should be only one root.
     }
 };
