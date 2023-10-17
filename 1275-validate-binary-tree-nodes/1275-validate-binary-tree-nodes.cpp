115daefde7b25f9994586386ad1195bf3dcf160d
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-10-17 10:44:43 +0530

    Time: 30 ms (89.82%), Space: 32.9 MB (66.72%) - LeetHub

--- 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.cpp
+++ 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.cpp
@@ -1,52 +1,52 @@
 class Solution {
 public:
     bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
-       int count = 0;
-       vector<int>parent(n, 0);
+    int edgeCount = 0;
+    vector<int> parentCount(n, 0);
 
-       for(int i = 0; i < n; i++) {
-            if(leftChild[i] != -1) {
-                parent[leftChild[i]]++;
+    for (int i = 0; i < n; i++) {
+        if (leftChild[i] != -1) {
+            parentCount[leftChild[i]]++;
 
-                if(parent[leftChild[i]] > 1) return false;
-                count++;
-            }
-
-            if(rightChild[i] != -1) {
-                parent[rightChild[i]]++;
-
-                if(parent[rightChild[i]] > 1) return false;
-                count++;
-            }
-       }
-
-       // count of edges should be n-1
-       if(count != n-1) return false;
-
-        //    parent should be exactly 1 or 0.
-        int root = -1;
-        for(int i = 0; i< n; i++) {
-            if(parent[i] == 0) {
-                if(root == -1) root = i;
-                else return false;
-            };
+            if (parentCount[leftChild[i]] > 1) return false;
+            edgeCount++;
         }
 
-        // bfs for checking visited node count.
-        queue<int>q;
-        q.push(root);
-        
-        count = 0;
+        if (rightChild[i] != -1) {
+            parentCount[rightChild[i]]++;
 
-        while(!q.empty()) {
-            int node = q.front();
-            q.pop();
-            count++;
-
-            if(leftChild[node] != -1) q.push(leftChild[node]);
-            if(rightChild[node] != -1) q.push(rightChild[node]);
+            if (parentCount[rightChild[i]] > 1) return false;
+            edgeCount++;
         }
-        return count == n;
-
     }
+
+    // The count of edges should be n-1.
+    if (edgeCount != n - 1) return false;
+
+    // Parent count should be exactly 1 or 0.
+    int root = -1;
+    for (int i = 0; i < n; i++) {
+        if (parentCount[i] == 0) {
+            if (root == -1) root = i;
+            else return false;
+        }
+    }
+
+    // Perform a breadth-first search to check the visited node count.
+    queue<int> q;
+    q.push(root);
+
+    int visitedCount = 0;
+
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        visitedCount++;
+
+        if (leftChild[node] != -1) q.push(leftChild[node]);
+        if (rightChild[node] != -1) q.push(rightChild[node]);
+    }
+
+    return visitedCount == n;
+}
 };
