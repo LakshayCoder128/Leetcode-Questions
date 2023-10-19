70f7cb61216f7d81cd6c3bffdec59ac7434b9a45
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-10-19 07:54:33 +0530

    Time: 0 ms (100%), Space: 6.9 MB (15.59%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.cpp
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    bool backspaceCompare(string S, string T) {
-        string resultS, resultT;
+    bool backspaceCompare(string s, string t) {
+              int i = s.length() - 1, j = t.length() - 1;
+        int skipS = 0, skipT = 0;
 
-        // Process string S
-        for (char ch : S) {
-            if (ch == '#' && !resultS.empty()) {
-                resultS.pop_back();
-            } else if (ch != '#') {
-                resultS.push_back(ch);
+        while (i >= 0 || j >= 0) { // While there may be chars in build(S) or build (T)
+            while (i >= 0) { // Find position of next possible char in build(S)
+                if (s[i] == '#') {skipS++; i--;}
+                else if (skipS > 0) {skipS--; i--;}
+                else break;
             }
-        }
-
-        // Process string T
-        for (char ch : T) {
-            if (ch == '#' && !resultT.empty()) {
-                resultT.pop_back();
-            } else if (ch != '#') {
-                resultT.push_back(ch);
+            while (j >= 0) { // Find position of next possible char in build(T)
+                if (t[j] == '#') {skipT++; j--;}
+                else if (skipT > 0) {skipT--; j--;}
+                else break;
             }
+            // If two actual characters are different
+            if (i >= 0 && j >= 0 && s[i] != t[j])
+                return false;
+            // If expecting to compare char vs nothing
+            if ((i >= 0) != (j >= 0))
+                return false;
+            i--; j--;
         }
-
-        return resultS == resultT;
+        return true;
     }
 };
