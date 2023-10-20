733376d4c836884f5fbffd2307d8b2f935e2b472
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-10-20 21:55:08 +0530

    Time: 7 ms (40.78%), Space: 23 MB (6.71%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,24 +1,27 @@
-class Solution {
-public:
-    vector<vector<int>>dp ;
-    void vi(int n){
-        vector<vector<int>>dp1(n+1,vector<int>(2,-1));
-        dp = dp1;
-    } 
-    int solve(vector<int>&p,int ind,bool b,int k){
-        if(ind >= p.size() || k <= 0) return 0;
+class Solution
+{
+    public:
+        vector<vector < int>> dp;
+
+    int solve(vector<int> &p, int ind, bool b, int k)
+    {
+        if (ind >= p.size() || k <= 0) return 0;
         int ans = 0;
-        if(dp[ind][b] != -1)return dp[ind][b];
-        if(b){
-            return dp[ind][b] = max(-p[ind] + solve(p,ind+1,!b,k),solve(p,ind+1,b,k));
+        if (dp[ind][b] != -1) return dp[ind][b];
+        if (b)
+        {
+            return dp[ind][b] = max(-p[ind] + solve(p, ind + 1, !b, k), solve(p, ind + 1, b, k));
         }
-        else {
-            return dp[ind][b] = max({p[ind] + solve(p,ind+1,!b,k-1),solve(p,ind+1,b,k)});
+        else
+        {
+            return dp[ind][b] = max({ p[ind] + solve(p, ind + 1, !b, k - 1),
+                solve(p, ind + 1, b, k) });
         }
-        // return max(sell, buy);
+       	// return max(sell, buy);
     }
-    int maxProfit(vector<int>& prices) {
-        vi(prices.size());
-        return solve(prices,0,1,INT_MAX);
+    int maxProfit(vector<int> &prices)
+    {
+        dp.resize(prices.size() + 1, vector<int> (2, -1));
+        return solve(prices, 0, 1, INT_MAX);
     }
 };
