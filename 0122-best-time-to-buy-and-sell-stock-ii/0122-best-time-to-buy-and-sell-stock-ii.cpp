b4af1a4384f23fc291a3e0723bf12a99e16d3567
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-10-21 09:13:17 +0530

    Time: 11 ms (17.51%), Space: 23.7 MB (6.76%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,27 +1,50 @@
 class Solution
 {
     public:
-        vector<vector < int>> dp;
-
-    int solve(vector<int> &p, int ind, bool b, int k)
+       
+    int Recursive(vector<int> &p, int ind, bool b, int k)
     {
         if (ind >= p.size() || k <= 0) return 0;
         int ans = 0;
-        if (dp[ind][b] != -1) return dp[ind][b];
+        
         if (b)
         {
-            return dp[ind][b] = max(-p[ind] + solve(p, ind + 1, !b, k), solve(p, ind + 1, b, k));
+            return max(-p[ind] + Recursive(p, ind + 1, !b, k), // bought it 
+                        Recursive(p, ind + 1, b, k));   // left it 
         }
         else
         {
-            return dp[ind][b] = max({ p[ind] + solve(p, ind + 1, !b, k - 1),
-                solve(p, ind + 1, b, k) });
+            return max({ p[ind] + Recursive(p, ind + 1, !b, k - 1), // sold it 
+                Recursive(p, ind + 1, b, k) }); // hold it
         }
-       	// return max(sell, buy);
+       	
     }
+
+    int RecursionWithMemoisation(vector<int> &p, int ind, bool canBuy, int k,vector<vector<int>>&dp)
+    {
+        if (ind >= p.size() || k <= 0) return 0;
+        int ans = 0;
+        
+        if(dp[ind][canBuy] != -1)return dp[ind][canBuy];
+        if (canBuy)
+        {
+            return dp[ind][canBuy] = max(-p[ind] + RecursionWithMemoisation(p, ind + 1, !canBuy, k,dp), // bought it 
+                        RecursionWithMemoisation(p, ind + 1, canBuy, k,dp));   // left it 
+        }
+        else
+        {
+            return dp[ind][canBuy] = max({ p[ind] + RecursionWithMemoisation(p, ind + 1, !canBuy, k - 1,dp), // sold it 
+                RecursionWithMemoisation(p, ind + 1, canBuy, k,dp) }); // hold it
+        }
+       	
+    }
+
     int maxProfit(vector<int> &prices)
     {
-        dp.resize(prices.size() + 1, vector<int> (2, -1));
-        return solve(prices, 0, 1, INT_MAX);
+        // return Recursive(prices, 0, 1, INT_MAX);
+        vector<vector<int>> dp(prices.size() + 1, vector<int> (2, -1));
+        return RecursionWithMemoisation(prices, 0, 1, INT_MAX,dp);
+
+
     }
 };
