d1ff81f6abb3fd648185db95b799b6da2616a201
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-10-21 10:32:13 +0530

    Time: 753 ms (5.41%), Space: 341.9 MB (5.32%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,39 +1,26 @@
-class Solution
-{
-    public:
-        int maxProfit(vector<int> &prices)
-        {
-           	// int ans = 0;
-           	// for(int i=0;i < prices.size()-1;i++){
-           	//     for(int j=i+1;j < prices.size();j++){
-           	//         ans =  max(ans,prices[j]-prices[i]);
-           	//     }
-           	// }
-           	// return ans;
+class Solution {
+public:
+    int bottomup(int K, vector<int> &p) {
+        vector<vector<vector<int> > > dp(p.size() + 1, vector<vector<int>>(3, vector<int>(K + 1, 0)));
+        
+        for (int ind = p.size() - 1; ind >= 0; ind--) {
+            for (int b = 0; b <= 1; b++) {
+                for (int k = 1; k <= K;
 
-           	// vector < int>dp(prices.size());
-           	// dp[prices.size()-1] = 0;
-           	// int maxi = prices[prices.size()-1];
-           	// int ans = 0;
-           	// for(int i=prices.size()-2;i>=0;i--){
-           	//     if(prices[i] >= maxi){dp[i]=0;maxi = prices[i];}
-           	//     else {dp[i] = maxi - prices[i]; ans = max(ans,dp[i]);}
-           	// }
-           	// return ans;
-
-            int maxi = prices[prices.size() - 1];
-            int ans = 0;
-            for (int i = prices.size() - 2; i >= 0; i--)
-            {
-                if (prices[i] >= maxi)
-                {
-                    maxi = prices[i];
-                }
-                else
-                {
-                    ans = max(ans, maxi - prices[i]);
+ k++) {
+                    if (b) {
+                        dp[ind][b][k] = max(-p[ind] + dp[ind + 1][!b][k], dp[ind + 1][b][k]);
+                    } else {
+                        dp[ind][b][k] = max(p[ind] + dp[ind + 1][!b][k - 1], dp[ind + 1][b][k]);
+                    }
                 }
             }
-            return ans;
         }
+        return dp[0][1][K];
+    }
+
+    int maxProfit(vector<int> &prices) {
+        int  k = 1;
+        return bottomup(k, prices);
+    }
 };
