2ab814e2e7a91e6e92fc478fd360fa4b3ef8232e
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-10-23 15:54:36 +0530

    Time: 0 ms (100%), Space: 6.4 MB (13.31%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if (n <= 0)
+        // Check if n is a power of 2
+        if (n <= 0 || (n & (n - 1)) != 0) {
             return false;
-        if (n == 1)
-            return true;
-        if (n % 4 == 0)
-            return isPowerOfFour(n / 4);
-        return false;
+        }
+        
+        // Check if the number of trailing 0s is even
+        return (n & 0x55555555) == n;
     }
 };
