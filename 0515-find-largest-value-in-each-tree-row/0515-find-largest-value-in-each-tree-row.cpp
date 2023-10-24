ff2546bca4e1e63da4d7a7632035b8dbc2524fcc
LakshayCoder128 <91819798+LakshayCoder128@users.noreply.github.com>
2023-10-24 13:14:43 +0530

    Time: 8 ms (68.56%), Space: 22.5 MB (50.91%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
@@ -2,23 +2,30 @@ class Solution {
 public:
     vector<int> largestValues(TreeNode* root) {
         vector<int> ans;
-        dfs(root, ans, 0);
-        return ans;
-    }
-    
-    void dfs(TreeNode* node, vector<int>& ans, int level) {
-        if (node == NULL) return;
+        if (root == NULL) return ans;
+        stack<pair<TreeNode*, int>> s; // Stack with node and corresponding level
+        s.push({root, 0});
         
-        // Ensure the 'ans' vector has a value for the current level.
-        if (level >= ans.size()) {
-            ans.push_back(node->val);
-        } else {
-            // Update 'ans' if the current value is larger.
-            ans[level] = max(ans[level], node->val);
+        while (!s.empty()) {
+            TreeNode* node = s.top().first;
+            int level = s.top().second;
+            s.pop();
+            
+            // Ensure the 'ans' vector has a value for the current level.
+            if (level >= ans.size()) {
+                ans.push_back(node->val);
+            } else {
+                // Update 'ans' if the current value is larger.
+                ans[level] = max(ans[level], node->val);
+            }
+            
+            if (node->left) {
+                s.push({node->left, level + 1});
+            }
+            if (node->right) {
+                s.push({node->right, level + 1});
+            }
         }
-        
-        // Recursively process children with an increased level.
-        dfs(node->left, ans, level + 1);
-        dfs(node->right, ans, level + 1);
+        return ans;
     }
 };
